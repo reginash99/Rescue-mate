67e66ce53e4a9f41eac91e1cb4600efc79133aa1
reginash99 <83776250+reginash99@users.noreply.github.com>
2025-08-11 00:34:38 +0200

    Add error handling for microphone access, adapted functionality and UI

--- frontend/src/components/Record.vue
+++ frontend/src/components/Record.vue
@@ -8,10 +8,11 @@
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <div :style="{ width: '100%', height: '100%' }">
+      
     <div ref="record_component"
       :style="{ width: '100%', height: '50%', position: 'relative', display: 'flex', alignItems: 'last baseline', justifyContent: 'center' }">
 
-      <button type="button" :class="{'btn btn-danger': !waitingForRecording, 'btn btn-secondary': waitingForRecording}" @click="isRecording ? stopRecording() : startRecording()" :disabled="waitingForRecording">
+      <button type="button" :class="{'btn btn-danger': !waitingForRecording, 'btn btn-secondary': waitingForRecording}" @click="isRecording ? stopRecording() : startRecording()" :disabled="waitingForRecording || error_message" >
         <div :style="{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10px' }">
           <svg v-if="!isRecording && !waitingForRecording" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
             class="bi bi-mic" viewBox="0 0 16 16">
@@ -41,19 +42,28 @@
       </button>
 
     </div>
-    <div id="recordingVideo" :style="{ visibility: isRecording ? 'visible' : 'hidden', width: '100%', height: '50%' }">
+
+    <div id="MicMessage" v-show="error_message" :style="{width: '100%', height: '30%', display:'flex', alignItems:'center', justifyContent:'center'}">
+      <div :style="{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '12px' , padding: '10px', border: 'solid red 3px', margin: '10px'}"> 
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="red" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+          <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z"/>
+          <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+        </svg>
+        {{error_message}}</div>
+    </div>
+
+    <div id="recording" :style="{ visibility: isRecording ? 'visible' : 'hidden', width: '100%', height: '50%' }">
 
       <div :style="{ width: '100%', height: '100%', display:'flex', alignItems:'baseline',justifyContent:'center' }">
 
         <video id="recordingVideo"
           src="./media/record_video_cropped.mp4" autoplay
-          loop muted :style="{maxWidth: '100%', height:'100%', objectFit:'fill'}"></video>
-        
+          loop muted :style="{maxWidth: '100%', height:'100%', objectFit:'fill'}">
+        </video>
 
       </div>
-
-      
     </div>
+    
     <!--this part is used for checking if the recording was generated successfully-->
     <!--<div>
   <audio controls :src="audio_url" v-if = "audio_generated"></audio>
@@ -73,6 +83,7 @@ import { defineEmits } from 'vue'
 const emit = defineEmits(['transcription'])
 const isRecording = ref(false)
 const waitingForRecording = ref(false);
+const error_message = ref(null);
 const clientWidth = ref(800);
 const clientHeight = ref(800);
 const Padding = {
@@ -99,17 +110,35 @@ let audio_generated = ref(false)
 let data = []
 let rec_timestamp = ref(null);
 
-function startRecording() {
+async function startRecording() {
+
   isRecording.value = true;
   console.log("Recording started")
 
   audio = [];
   data = []; // Clear previous data
+  if(recorder){
   recorder.start();
-
+  
   if (recorder.state == "recording") {
     rec_timestamp = new Date().toLocaleString('de-DE', timestamp_format)
   }
+}
+else{
+  try{
+   await createRecorder();
+    console.log("Recorder has to be created first")
+    recorder.start();
+  
+  if (recorder.state == "recording") {
+    rec_timestamp = new Date().toLocaleString('de-DE', timestamp_format)
+  }
+  }
+  catch (error){
+    console.error("Error creating recorder: ", error);
+    error_message.value = "Error creating recorder. Please check your microphone settings."
+  }
+}
 }
 
 function stopRecording() {
@@ -168,17 +197,14 @@ async function sentAudio() {
    }
 }
 
-onMounted(() => {
-  if (record_component.value) {
-    clientWidth.value = record_component.value.clientWidth;
-  }
-
-  if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia(
+function createRecorder() {
+  error_message.value = null;
+  return navigator.mediaDevices.getUserMedia(
+      
       {
         audio: true
-      }
-    )
+      })
+    
       .then((stream) => {
 
         let options = { mimeType: "audio/webm;codecs=opus" };
@@ -196,9 +222,46 @@ onMounted(() => {
         }
       })
       .catch((error) => {
+        error_message.value = "Microphone access denied. Please enable microphone access in your browser settings."
+        console.error("Error occured: ", error)
+        
+      })
+}
+
+navigator.permissions.query({ name: 'microphone' })
+    .then((permission) => {
+      
+      if (permission.state === 'denied'){
+        error_message.value = "Microphone access denied. Please enable microphone access in your browser settings"
+      }
+      else if (permission.state === 'prompt'){
+        error_message.value = "Please accept microphone access to use our application"
+       
+        navigator.mediaDevices.getUserMedia({
+        audio: true
+      }
+      ).then((stream) => {
+        error_message.value = null;  
+      
+      }).catch((error) => {
+        error_message.value = "Microphone access denied. Please enable microphone access in your browser settings"
         console.error("Error occured: ", error)
       })
+      }
+      else {
+        error_message.value = null;
+        createRecorder();
+      }
+  })
+onMounted(() => {
+  if (record_component.value) {
+    clientWidth.value = record_component.value.clientWidth;
   }
+  //createRecorder();
 
-});
+
+})
+
+
+  
 </script>
