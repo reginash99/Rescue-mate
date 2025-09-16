5bec3bd10696e6e26848cac4a3dfc280c8e788a4
reginash99 <83776250+reginash99@users.noreply.github.com>
2025-09-17 00:09:58 +0200

    Enhance database interaction by updating select_records to retrieve specific fields and adding get_latest_id and insert_intermediate_record functions for improved data management.

--- backend/db.py
+++ backend/db.py
@@ -69,7 +69,7 @@ def select_records():
      
             if cur.fetchone()[0]:
                 print("table exists")
-                cur.execute("select * from transcription order by id desc;")
+                cur.execute("select id, timestamp,transcription,status from transcription order by id desc;")
                 conn.commit()
 
                 records = cur.fetchall()
@@ -140,6 +140,58 @@ def get_id(timestamp, transcription,status):
 
                 
     return id[0][0]
-    
+
+def get_latest_id():
+    with psycopg.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        host=os.getenv("HOST"),
+        port=os.getenv("PORT")
+    ) as conn:
+
+        with conn.cursor() as cur:
+            table_name = 'transcription'
+            cur.execute("select exists(select from information_schema.tables where table_name=%s)",(table_name,))
+            
+     
+            if cur.fetchone()[0]:
+                print("table exists")
+                cur.execute("select max(id) from transcription;")
+                conn.commit()
+
+                id = cur.fetchall()
+
+                
+    return id[0][0]    
+
+def insert_intermediate_record(transcription, column_index,id):
+    with psycopg.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        host=os.getenv("HOST"),
+        port=os.getenv("PORT")
+    ) as conn:
+         with conn.cursor() as cur:
+            table_name = 'transcription'
+            cur.execute("select exists(select from information_schema.tables where table_name=%s)",(table_name,))
+
+            match column_index:
+                case 1:
+                    cur.execute("update transcription set raw_transcr = %s where id = %s", (transcription,id,))
+                case 2:
+                    cur.execute("update transcription set bp_preemp_transcr = %s where id = (select max(id) from transcription);", (transcription,))
+                case 3:
+                    cur.execute("update transcription set mamba_bp_transcr  = %s where id = (select max(id) from transcription);", (transcription,))
+                case 4:
+                    cur.execute("update transcription set mamba_bp_preemp_transcr = %s where id = (select max(id) from transcription);", (transcription,))
+                case 5:
+                    cur.execute("update transcription set mamba_bp_preemp_deepfilternet_transcrp = %s where id = (select max(id) from transcription);", (transcription,))
+                case _:
+                    print("No valid column index provided.")
+                    return
 #insert_record('2025-09-07 00:12:00', 'This is a test transcription.',False) # just a test that the inserting function works
 #delete_records()
+
+#insert_intermediate_record('This is a test raw transcription to test the id.',1)
