4a162931bade7cf213d836a613c21937300ed814
Kashf Jahangir <kashf.jahangir.pk@gmail.com>
2025-09-18 18:41:15 +0200

    frontend package remove

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,15 +9,5 @@ import 'vue-map-ui/dist/normalize.css';
 import 'vue-map-ui/dist/style.css';
 import 'vue-map-ui/dist/theme-all.css';
 
-//necessay for resizing components
-// @ts-ignore
-import VueDraggableResizable from 'vue-draggable-resizable'
-import 'vue-draggable-resizable/style.css'
 
-export default {
-  components: { VueDraggableResizable }
-}
-
-createApp(App)
-    .component("vue-draggable-resizable", VueDraggableResizable)
-    .mount('#app');
+createApp(App).mount('#app');
